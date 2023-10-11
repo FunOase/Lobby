0dd1adf429da3ff25376c8d30592478347b7c779
RappyTV <rappytv@rappytv.com>
2023-10-11 11:23:54 +0200

    Add some events to the PlayerListener

--- src/main/java/com/rappytv/lobby/listeners/PlayerListener.java
+++ src/main/java/com/rappytv/lobby/listeners/PlayerListener.java
@@ -1,17 +1,19 @@
 package com.rappytv.lobby.listeners;
 
 import com.rappytv.lobby.LobbyPlugin;
+import com.rappytv.lobby.inventories.TeleporterInventory;
 import com.rappytv.lobby.items.Teleporter;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.inventory.Inventory;
 
 public class PlayerListener implements Listener {
@@ -63,21 +65,51 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerDamage(EntityDamageByEntityEvent e) {
-        if (!(e.getEntity() instanceof Player) && !(e.getDamager() instanceof Player)) return;
-        Player player = (Player) e.getEntity();
-        Player attacker = (Player) e.getDamager();
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof Player) && !(event.getDamager() instanceof Player)) return;
+        Player player = (Player) event.getEntity();
+        Player attacker = (Player) event.getDamager();
         if(attacker.hasPermission("lobby.attack")) {
             if(player.hasPermission("lobby.attack.block"))
-                e.setCancelled(true);
+                event.setCancelled(true);
         } else
-            e.setCancelled(true);
+            event.setCancelled(true);
     }
 
     @EventHandler
-    public void onDeath(PlayerDeathEvent e) {
-        e.setDeathMessage("");
-        e.setDroppedExp(0);
-        e.getDrops().clear();
+    public void onDeath(PlayerDeathEvent event) {
+        event.setDeathMessage("");
+        event.setDroppedExp(0);
+        event.getDrops().clear();
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+        if(!player.hasPermission("lobby.items.drop")) {
+            Item drop = event.getItemDrop();
+            drop.getItemStack().setAmount(0);
+            event.setCancelled(true);
+            setPlayerInventory(player);
+        }
+    }
+
+    @EventHandler
+    public void onItemPickup(PlayerPickupItemEvent event) {
+        event.setCancelled(!event.getPlayer().hasPermission("lobby.items.pickup"));
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        Teleporter item = new Teleporter();
+
+        if(event.getItem() == null || event.getItem().getItemMeta() == null) return;
+        if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if(event.getItem().getItemMeta().getDisplayName().equalsIgnoreCase(item.getItemMeta().getDisplayName()) && player.hasPermission("lobby.page.default")) {
+                player.openInventory(TeleporterInventory.get(player));
+            }
+        }
     }
 }
